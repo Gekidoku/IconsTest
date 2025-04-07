10799ad55f73a4cc35c8bdc1f02d4eed9ade77c4
reinierasberg <tom_sproet@live.nl>
2025-04-07 09:25:13 +0200

    first commit

---
+++ IconsTest/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace IconsTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		//Capitalized L is from xcassets lowercase l is from resources/mauiimage
+		 var testitem = new ToolbarItem()
+            {
+                
+                Order = ToolbarItemOrder.Primary,
+                Priority = 1
+                ,IconImageSource = ImageSource.FromFile("Licht.png")
+            };
+            
+            ToolbarItems.Add(testitem);
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ IconsTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IconsTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
